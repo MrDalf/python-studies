3dfc10964e05c357a4ada898ee1c844349343ffb
MrDalf <vitorc.dalefi@gmail.com>
2025-08-14 18:32:55 -0300

    Add content/basics/basic_math.py structure

---
+++ content/basics/basic_math.py
@@ -0,0 +1,41 @@
+"""
+Basic mathematical operations can be performed in Python using the symbols below:
+
+SUM (+)
+
+SUBTRACTION (-)
+
+DIVISION (/)
+
+MULTIPLICATION (*)
+
+POWER (**)
+
+REMAIN (%)
+
+Python will follow the same order of operations as math.
+"""
+
+# Examples
+
+# Sum
+print(1+2)
+
+# Subtraction
+print(54-8)
+# Division
+print(100/10)
+
+# Multiplication
+print(3*4)
+
+# Power
+print(2**10)
+
+# Remain
+print(10%3)
+
+print(1+2**2)
+print((1+2)**2)
+print(2*2**2)
+print((2*2)**2)
