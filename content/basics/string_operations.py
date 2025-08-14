5a12356c99c326d75a8aadb1ed754a85a85a318f
MrDalf <vitorc.dalefi@gmail.com>
2025-08-14 18:46:40 -0300

    Add string_operations.py to basics

---
+++ content/basics/string_operations.py
@@ -0,0 +1,13 @@
+"""
+You can concatenate different texts using the "+" sign between Strings.
+
+You can check if a given substring is present in a String using "in"
+
+You can format texts with variables using "f" before the String and instead of variables using them between "{}"
+
+"""
+
+name =  'João'
+print("His name is " + name)
+print("His name is" , name)
+print(f"His name is {name}")
